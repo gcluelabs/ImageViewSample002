68f85b67bb1732538805755827ca53ba3c555768
hoshit <hoshi@gclue.jp>
2013-04-03 16:44:03 +0900

    初回コミット.

--- src/com/example/layoutsample/LayoutSample.java
+++ src/com/example/layoutsample/LayoutSample.java
@@ -4,21 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class LayoutSample extends Activity {
-
+	/**
+	 * Text01
+	 */
+	TextView mText01;
+	
+	/**
+	 * Button01
+	 */
+	Button mButton01;
+	
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Button mButton01 = (Button)findViewById(R.id.Button01);
+		
+		// Text01を取り込む
+		mText01 = (TextView)findViewById(R.id.Text01);
+		
+		// Button01を取り込む
+		mButton01 = (Button)findViewById(R.id.Button01);
 		// Button01のイベント処理
 		mButton01.setOnClickListener(new View.OnClickListener() {
-			// Button01が押された場合の処理
+			// Bottan01が押された場合の処理
 			public void onClick(View v) {
-				Toast.makeText(LayoutSample.this, "Button01が押されました。", Toast.LENGTH_SHORT).show();
-
+				mText01.setText("Button01が押されました。");
 			}
 		});
 	}
