c22d09c60cdc05296387c1e72c42dad02cdc8ac0
hoshit <hoshi@gclue.jp>
2013-04-03 16:32:02 +0900

    初回コミット.

--- gen/com/example/layoutsample/R.java
+++ gen/com/example/layoutsample/R.java
@@ -25,8 +25,9 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Button01=0x7f080000;
-        public static final int action_settings=0x7f080001;
+        public static final int Button01=0x7f080001;
+        public static final int Text01=0x7f080000;
+        public static final int action_settings=0x7f080002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -37,6 +38,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050001;
+        public static final int defaultText=0x7f050005;
         public static final int hello=0x7f050000;
         public static final int hello_world=0x7f050003;
         public static final int txtButton01=0x7f050004;
