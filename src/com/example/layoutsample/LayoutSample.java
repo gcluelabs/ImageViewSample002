6642618520d406a8664e707f09240bc2bf290585
hoshit <hoshi@gclue.jp>
2013-04-03 17:49:02 +0900

    初回コミット.

--- src/com/example/layoutsample/LayoutSample.java
+++ src/com/example/layoutsample/LayoutSample.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LayoutSample extends Activity {
@@ -25,11 +26,25 @@ public class LayoutSample extends Activity {
 	 */
 	EditText mEditText01;
 
+	/**
+	 * ImageView01
+	 */
+	ImageView mImageView01;
+
+	/**
+	 * 画像の表示非表示のフラグ
+	 */
+	boolean imageFlag;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		// ImageViewを取り込む
+		mImageView01 = (ImageView) findViewById(R.id.imageView1);
+		imageFlag = true;
 
 		// EditTextを取り込む
 		mEditText01 = (EditText) findViewById(R.id.EditText01);
@@ -45,6 +60,14 @@ public class LayoutSample extends Activity {
 			public void onClick(View v) {
 				String input = mEditText01.getText().toString();
 				mText01.setText(input);
+				if (imageFlag) {
+					mImageView01.setVisibility(View.INVISIBLE);
+					imageFlag = false;
+				} else {
+					mImageView01.setVisibility(View.VISIBLE);
+					imageFlag = true;
+				}
+
 			}
 		});
 	}
