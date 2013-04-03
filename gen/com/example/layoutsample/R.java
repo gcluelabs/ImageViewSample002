f49bc50ac688d26755a9e499cf3bfcaf07911154
hoshit <hoshi@gclue.jp>
2013-04-03 17:00:04 +0900

    初回コミット.

--- gen/com/example/layoutsample/R.java
+++ gen/com/example/layoutsample/R.java
@@ -25,9 +25,10 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Button01=0x7f080001;
+        public static final int Button01=0x7f080002;
+        public static final int EditText01=0x7f080001;
         public static final int Text01=0x7f080000;
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/example/layoutsample/LayoutSample.java
+++ src/com/example/layoutsample/LayoutSample.java
@@ -3,36 +3,48 @@ package com.example.layoutsample;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LayoutSample extends Activity {
+
 	/**
 	 * Text01
 	 */
 	TextView mText01;
-	
+
 	/**
 	 * Button01
 	 */
 	Button mButton01;
-	
+
+	/**
+	 * EditText01
+	 */
+	EditText mEditText01;
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
+		// EditTextを取り込む
+		mEditText01 = (EditText) findViewById(R.id.EditText01);
+
 		// Text01を取り込む
-		mText01 = (TextView)findViewById(R.id.Text01);
-		
+		mText01 = (TextView) findViewById(R.id.Text01);
+
 		// Button01を取り込む
-		mButton01 = (Button)findViewById(R.id.Button01);
+		mButton01 = (Button) findViewById(R.id.Button01);
 		// Button01のイベント処理
-		mButton01.setOnClickListener(new View.OnClickListener() {
+		mButton01.setOnClickListener(new OnClickListener() {
 			// Bottan01が押された場合の処理
 			public void onClick(View v) {
-				mText01.setText("Button01が押されました。");
+				String input = mEditText01.getText().toString();
+				mText01.setText(input);
 			}
 		});
 	}
